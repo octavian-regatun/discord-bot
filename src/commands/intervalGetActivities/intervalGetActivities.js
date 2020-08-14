a2a7cb18294b165f2aaecfb424b1eca54296a141
Octavian Regatun <octavian.regatun@outlook.com>
2020-08-14 21:58:16 +0300

    add: intervalGetActivities

---
+++ src/commands/intervalGetActivities/intervalGetActivities.js
@@ -0,0 +1,13 @@
+const { runGetActivities } = require('../getActivities');
+
+let interval;
+
+module.exports.startGetActivities = (message, timer) => {
+  interval = setInterval(
+    (message) => {
+      runGetActivities(message, false);
+    },
+    timer,
+    message
+  );
+};
