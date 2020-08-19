59a11945d4a938cf029eeebd6340195fa21032e0
Octavian Regatun <octavian.regatun@outlook.com>
2020-08-19 19:54:01 +0300

    delete: all .js files

--- src/commands/stopGetActivities.js
+++
@@ -1,9 +0,0 @@
-module.exports = (message) => {
-  const {
-    stopGetActivities,
-  } = require('./intervalGetActivities/intervalGetActivities');
-
-  stopGetActivities;
-
-  message.reply('Get activity interval stopped');
-};
